dee1abd01f4e59d65f3425618bf0af19a0547be0
Miguel Almonte <miguelalmonte@neoscloudllc.com>
2016-04-27 17:42:36 -0400

    Upgrade Android studio from 2.0 to 2.1

--- app/src/main/java/com/malmonte/sunshine/app/MainActivity.java
+++ app/src/main/java/com/malmonte/sunshine/app/MainActivity.java
@@ -9,8 +9,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends ActionBarActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,6 +61,21 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            ArrayList<String> weekForecast = new ArrayList<>();
+            weekForecast.add("Today - Sunny - 88/63");
+            weekForecast.add("Tomorrow - Foggy - 70/46");
+            weekForecast.add("Weds - Cloudy - 72/63");
+            weekForecast.add("Thurs - Rainy - 64/51");
+            weekForecast.add("Fri - Foggy - 70/46");
+            weekForecast.add("Sat - Sunny - 76/68");
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                    R.layout.list_item_forecast,
+                    R.id.list_item_forecast_textview,
+                    weekForecast);
+            ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+            listView.setAdapter(adapter);
+
             return rootView;
         }
     }
