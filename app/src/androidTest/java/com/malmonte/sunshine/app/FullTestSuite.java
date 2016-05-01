63c570811e1928cf0f3ae13aab3c27cc3220cbcb
Miguel Almonte <miguel.almonte1@gmail.com>
2016-05-01 14:14:26 -0400

    Add LocationEntry column names

--- app/src/androidTest/java/com/malmonte/sunshine/app/FullTestSuite.java
+++ app/src/androidTest/java/com/malmonte/sunshine/app/FullTestSuite.java
@@ -3,5 +3,19 @@ package com.malmonte.sunshine.app;
 /**
  * Created by malmonte on 5/1/16.
  */
-public class FullTestSuite {
+
+import android.test.suitebuilder.TestSuiteBuilder;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class FullTestSuite extends TestSuite {
+    public static Test suite() {
+        return new TestSuiteBuilder(FullTestSuite.class)
+                .includeAllPackagesUnderHere().build();
+    }
+
+    public FullTestSuite() {
+        super();
+    }
 }
